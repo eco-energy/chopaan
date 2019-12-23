a1ee3b7abcf57304c1f42e8a9c51da0605fac86b
Faez Shakil <faez@ecoenergyfinance.org>
2019-12-23 20:06:03 +0500

    wip

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,6 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE OverloadedLabels #-}
 {-# LANGUAGE TypeApplications #-}
 
@@ -39,6 +38,9 @@ import Control.Exception (Handler (..), IOException, catches)
 import Control.Monad (forever, when)
 import Control.Concurrent (threadDelay)
 import qualified Data.ByteString.Lazy as BL
+import qualified Data.Map.Strict as Map
+import Control.Concurrent.STM
+import Control.Concurrent
 
 
 eTR :: NM.EnergyTransactionRequest
@@ -71,18 +73,69 @@ kbtz = Just "PILOT"
 -- We obtain the list of nodes for a particular kibbutz by using the kibbutz name and querying AWS Iot for all thing names
 -- for things which belong to that kibbutz.
 
+----------------------------------------------------------------------------------
+-- Metric Tracking
+
+type WattSeconds = Double
+
+type S = Double
+
+data Metrics = Metrics
+  { transmittedIn :: WattSeconds
+  , transmittedOut :: WattSeconds
+  , consumed :: WattSeconds
+  , generated :: WattSeconds
+  , tDiff :: S
+  } deriving (Eq, Show, Ord)
+
+instance Semigroup Metrics where
+  m0 <> m1 = Metrics tIn tOut c g newTimeDiff
+    where
+      [tIn, tOut, c, g, newTimeDiff] = map (\t -> (sum $ map t ms)) ts 
+      ms = [m0, m1]
+      ts = [transmittedIn, transmittedOut, consumed, generated, tDiff]
+
+instance Monoid Metrics where
+  mempty = Metrics 0 0 0 0 0
+
+processES :: S -> EnergyState -> Metrics
+processES prevTime es = Metrics tIn tOut c g tdiff
+  where
+    tIn :: WattSeconds
+    tIn = es ^. batteryVoltage * es ^. gridToBatteryCurrent 
+    tOut :: WattSeconds
+    tOut = es ^. batteryVoltage * es ^. batteryToGridCurrent
+    c :: WattSeconds
+    c = es ^. batteryVoltage * es ^. batteryToLoadCurrent
+    g :: WattSeconds
+    g = es ^. batteryVoltage * es ^. solarInputCurrent * timeInSeconds
+    timeInSeconds = (fromIntegral $ es ^. cpuTime) / (1000 * 60) -- milliSToSeconds
+    tdiff = timeInSeconds - prevTime
+
+type NodeStates = Map.Map NodeID Metrics
+
+updateNodeState :: NodeStates -> NodeID -> EnergyState -> NodeStates
+updateNodeState ns n es = Map.adjust updateMetric n ns
+  where
+    updateMetric = (<> processES s es)
+    s = tDiff $ ns Map.! n
+
+----------------------------------------------------------------------------------
 
 type StateTopic = Text.Text
 type ControlTopic = Text.Text
 
 type ThingName = Text.Text
 
+newtype NodeID = NodeID { unNodeID :: ThingName } deriving (Eq, Show, Ord)
 
-getThings :: IO [Iot.ThingAttribute]
-getThings = do
+
+
+getThings :: Text.Text -> IO [Iot.ThingAttribute]
+getThings thingTypeName = do
   let
     iiot = Iot.ioT{_svcPrefix="execute-api"} :: Service 
-    ttn = (Just "kibbutz-pilot-node") :: Maybe Text.Text
+    ttn = (Just thingTypeName) :: Maybe Text.Text
   lgr <- newLogger Trace stdout
   env <- newEnv Discover <&> set envLogger lgr . set envRegion Singapore <&> configure iiot
   runResourceT . runAWST env $ do
@@ -107,10 +160,15 @@ nameToTopics name = (st, ct)
     prefix = "/kibbutz/node/"
     n = Text.replace ":" "" name
 
+stateTopicToNodeID :: Text.Text -> NodeID
+stateTopicToNodeID t = NodeID n
+  where
+    n = Text.replace "/state" "" $ Text.replace "/kibbutz/node" "" t
+    
 
 -- https://stackoverflow.com/questions/40081508/how-to-provide-a-client-certificate-to-http-client-tls
-mkTLSSettings :: IO TLSSettings
-mkTLSSettings = do
+mkTLSSettings :: Text.Text -> Text.Text -> IO TLSSettings
+mkTLSSettings hostName name = do
   creds <- either (error "couldn't read cert") Just <$> credentialLoadX509 cert key
   let
     hooks = def { onCertificateRequest = \_ -> return creds
@@ -124,14 +182,16 @@ mkTLSSettings = do
   where
     cert = "certs/chopaan.cert.pem"
     key = "certs/chopaan.private.key.pem"
-    hostName = "mqtts://a1e7lyi19kctcn-ats.iot.ap-southeast-1.amazonaws.com"
-    name = "chopaan-pilot"
+
+initMonitorState :: [ThingName] -> NodeStates
+initMonitorState ts = Map.fromList [((NodeID t), mempty) | t <- ts]
 
 
 main :: IO ()
 main = do
-  things <- getThings
-  tlsConf <- mkTLSSettings
+  things <- getThings thingTypeName
+  tlsConf <- mkTLSSettings mqttURI connID
+  monitorState <- atomically $ newTVar initMonitorState
   let
     topics = (fmap nameToTopics) <$> map thingName things
     stopics :: [(Text.Text, MQ.SubOptions)]
@@ -139,36 +199,49 @@ main = do
       where
         ts = (map ((fromMaybe "NoTopic") . fmap fst) topics)
         subopts = (repeat MQ.subOptions)
-    (Just uri) = parseURI $ "mqtts://a1e7lyi19kctcn-ats.iot.ap-southeast-1.amazonaws.com" <> "#" <> "chopaan-pilot" 
+    cb _ t m p =  print (t, msg parsed, l)
+      where
+        nodeId = stateTopicToNodeID t
+        msg (Left a) = show a
+        msg (Right s) = updateNodeState s
+        parsed :: Either String EnergyState
+        parsed = decodeMessage $ toStrict m
+        l = BL.length m
+        toStrict = BS.concat . BL.toChunks
+        
+    (Just uri) = parseURI $ mqttURI <> "#" <> connID 
     conf = MQ.mqttConfig
            { MQ._protocol=MQ.Protocol311
-           , MQ._connID="chopaan-pilot"
+           , MQ._connID=connID
            , MQ._msgCB=MQ.SimpleCallback cb
            , MQ._connectTimeout=18000000000
            , MQ._tlsSettings=tlsConf}
 
   forever $ catches (go conf uri stopics) [Handler (\(ex :: MQ.MQTTException) -> handler (show ex))]
   where
+    connID = "chopaan-pilot" :: Text.Text
+    mqttURI = "mqtts://a1e7lyi19kctcn-ats.iot.ap-southeast-1.amazonaws.com" :: Text.Text
+    thingTypeName = "kibbutz-pilot-node" :: Text.Text
     go c u ts = do
       mc <- MQ.connectURI c u
       print =<< MQ.subscribe mc ts []
       pub mc
       MQ.waitForClient mc
-    cb _ t m p =  print (t, msg parsed, l)
-      where
-        msg (Right s) = show s
-        msg (Left a) = show a
-        parsed :: Either String EnergyState
-        parsed = decodeMessage $ toStrict m
-        l = BL.length m
-        toStrict = BS.concat . BL.toChunks
+    
 
     handler e = putStrLn ("ERROR :" <> e) >> threadDelay 1000000
     pub :: MQ.MQTTClient -> IO ()
     pub c = do
-      forever $ MQ.publish c "/kibbutz/node/3c71bf644520/control" pMsg False >> threadDelay 10000
+      forever $ MQ.publish c "/kibbutz/node/3c71bf644520/control" pMsg False >> threadDelay 10000000
       where
-        pMsg = (BL.fromStrict . BS.init) (encodeMessage meshFrame)
+        pMsg = BL.fromStrict $ encodeMessage meshFrame
+
+data ChopaanOpts = ChopaanOpts
+  { mqttURI :: Text.Text
+  , connID :: Text.Text
+  , thingTypeName :: Text.Text
+  }
+
 {--
 import Import
 import Run
