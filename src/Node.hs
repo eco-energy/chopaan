39e19b35ffc7c113cd1cf55a863acdc615b00721
Faez Shakil <faez@ecoenergyfinance.org>
2020-02-03 19:01:30 +0500

    added functions for checking NodeStream aggregations

--- src/Node.hs
+++ src/Node.hs
@@ -8,9 +8,16 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-module Node (runNodeMonitor, NodeId(..), defaultES, NodeS, NodeMetrics(..), defNodeS) where
-
-
+module Node (
+  -- functional export
+  runNodeMonitor
+  -- data constructors
+  , EnergyState, NodeId(..), NodeS, NodeMetrics(..), EnergyBalance
+  -- calculations exported for tests
+  , stored, demand, loss, lastWait, energyStream, powerStream
+  -- default builders
+  , defaultES, defNodeS
+  ) where
 import qualified Data.Time as Time
 import Data.Time.Clock.POSIX
 
@@ -30,6 +37,8 @@ import qualified Streamly.Prelude as S
 import qualified Data.Set as Set 
 
 import Data.ProtoLens (Message, defMessage)
+
+import Data.Hashable
 ----------------------------------------------------------------------------------
 -- Metric Tracking
 
@@ -43,6 +52,9 @@ type Demand = Double
 newtype NodeId a = NodeId { unNodeId :: a } deriving (Eq, Show, Ord, Data, Generic)
 
 instance (Show a) => PPT.CellValueFormatter (NodeId a)
+
+instance (Hashable a) => Hashable (NodeId a) where
+  hashWithSalt n (NodeId a) = hashWithSalt n a
 
 -- Episodic Metrics
 
@@ -102,89 +114,31 @@ defNodeS = NodeMetrics 0 0 0 0 mempty mempty defaultES
 type NodeS = NodeMetrics WattSeconds Watts
 
 
-runNodeMonitor :: (Eq a, MonadAsync m, IsStream t, Applicative (t m)) => Time.UTCTime -> NodeId a -> t m (NodeId a, EnergyState) -> t m NodeS
+runNodeMonitor :: (Eq a, Monad m, IsStream t, Applicative (t m)) => Time.UTCTime -> NodeId a -> t m (NodeId a, EnergyState) -> t m NodeS
 runNodeMonitor initTime nodeId stream =
   let
     t = lastWait initTime nodeStream
     energyBalance = S.map snd $ energyStream initTime nodeStream
     power = powerStream nodeStream
     nms = NodeMetrics <$> t
-         <*> loss energyBalance
-         <*> stored energyBalance
-         <*> demand energyBalance
-         <*> power
-         <*> energyBalance
-         <*> nodeStream
+      <*> loss energyBalance
+      <*> stored energyBalance
+      <*> demand energyBalance
+      <*> power
+      <*> energyBalance
+      <*> nodeStream
   in nms
   where
     nodeStream = S.filter (\a-> fst a == nodeId) stream & S.map snd
-    utcTNow :: EnergyState -> Time.UTCTime 
-    utcTNow es = posixSecondsToUTCTime $ fromIntegral $ es ^. cpuTime
-
-    stored :: (IsStream t, Monad m) => t m EnergyBalance -> t m WattSeconds
-    stored es = S.scanl' stored' 0 es
-      where
-        stored' :: WattSeconds -> EnergyBalance -> WattSeconds
-        stored' s' (EnergyBalance{..}) = s' +
-                                         (generated + ((withLossFrac cLoss) txIn))
-                                         - ((withLossFrac dLoss txOut) + (withLossFrac cLoss) consumed)
-        withLossFrac a = ((1 + a) *)
-        cLoss = 0.01
-        dLoss = 0.1
-
-    demand :: (IsStream t, Monad m) => t m EnergyBalance -> t m WattSeconds
-    demand es = S.map demand' es
-      where
-        demand' :: EnergyBalance -> WattSeconds
-        demand' EnergyBalance{..} = (consumed - generated)
-
-    -- Imagine we're getting the energy audits of all the nodes for a certain window,
-    -- and that we have to calculate thesum txIn across all nodes
-    loss :: (IsStream t, Monad m) => t m EnergyBalance -> t m WattSeconds
-    loss es = (S.scanl' nLoss 0 es)
-      where
-        nLoss :: WattSeconds -> EnergyBalance -> WattSeconds
-        nLoss l' (EnergyBalance {txOut, txIn}) = l' + (txOut - txIn)
-
-    lastWait :: (IsStream t, Monad m) => Time.UTCTime -> t m EnergyState -> t m Time.NominalDiffTime
-    lastWait t es = S.map snd $ S.scanl' sf (t, 0 :: Time.NominalDiffTime) es
-      where
-        sf :: (Time.UTCTime, Time.NominalDiffTime) -> EnergyState -> (Time.UTCTime, Time.NominalDiffTime)
-        sf (ptime, _) e = (utcTNow e, Time.diffUTCTime (utcTNow e) ptime)
 
 
-    energyStream :: (IsStream t, Monad m) => Time.UTCTime -> t m EnergyState -> t m (Time.UTCTime, EnergyBalance)
-    energyStream t es = S.scanl' energyAtT (t, mempty) es
-      where 
-        energyAtT :: (Time.UTCTime, EnergyBalance) -> EnergyState -> (Time.UTCTime, EnergyBalance)
-        energyAtT (prevT, prevEb) es' = (tNow, prevEb <> eb)
-          where
-            eb = EnergyBalance txIn' txOut' cnsm' gen'
-            txIn' :: WattSeconds
-            txIn' = integrate $ p batteryVoltage gridToBatteryCurrent 
-            txOut' :: WattSeconds
-            txOut' = integrate $ p batteryVoltage batteryToGridCurrent
-            cnsm' :: WattSeconds
-            cnsm' = integrate $ p batteryVoltage batteryToLoadCurrent
-            gen' :: WattSeconds
-            gen' = integrate $ p batteryVoltage solarInputCurrent
-            p v i = es' ^. v * es' ^. i
-            integrate p' = p' * delT
-            delT = realToFrac $ Time.diffUTCTime tNow prevT
-            tNow = utcTNow es'
+newtype Monitor = Monitor { runMonitor :: NodeS } deriving (Eq, Ord, Show)
 
-    powerStream :: (IsStream t, (Monad m)) => t m EnergyState -> t m (Power Watts)
-    powerStream = S.map powerAtT
-      where
-        powerAtT :: EnergyState -> Power Watts
-        powerAtT es = Power txIn' txOut' cnsm' gen'
-          where
-            txIn' = p batteryVoltage gridToBatteryCurrent 
-            txOut' = p batteryVoltage batteryToGridCurrent
-            cnsm' = p batteryVoltage batteryToLoadCurrent
-            gen' = p batteryVoltage solarInputCurrent
-            p v i = es ^. v * es ^. i
+updateMonitor :: Monitor -> NodeS -> Monitor
+updateMonitor (Monitor _) n = (Monitor n)
 
+utcTNow :: EnergyState -> Time.UTCTime 
+utcTNow es = posixSecondsToUTCTime $ fromIntegral $ es ^. cpuTime
 
 defaultES :: EnergyState
 defaultES = defMessage
@@ -195,3 +149,68 @@ defaultES = defMessage
                & gridToBatteryCurrent .~ 0
                & solarInputCurrent .~ 0
                & dutyCycle .~ 0
+               & cpuTime .~ 0
+
+stored :: (IsStream t, Monad m) => t m EnergyBalance -> t m WattSeconds
+stored es = S.scanl' stored' 0 es
+  where
+    stored' :: WattSeconds -> EnergyBalance -> WattSeconds
+    stored' s' (EnergyBalance{..}) = s' +
+                                     (generated + ((withLossFrac cLoss) * txIn))
+                                     - ((withLossFrac dLoss * txOut) + (withLossFrac cLoss) * consumed)
+    withLossFrac a = (1 + a)
+    cLoss = 0.01
+    dLoss = 0.1
+
+demand :: (IsStream t, Monad m) => t m EnergyBalance -> t m WattSeconds
+demand es = S.map demand' es
+  where
+    demand' :: EnergyBalance -> WattSeconds
+    demand' EnergyBalance{..} = (consumed - generated)
+
+-- Imagine we're getting the energy audits of all the nodes for a certain window,
+-- and that we have to calculate thesum txIn across all nodes
+loss :: (IsStream t, Monad m) => t m EnergyBalance -> t m WattSeconds
+loss es = (S.scanl' nLoss 0 es)
+  where
+    nLoss :: WattSeconds -> EnergyBalance -> WattSeconds
+    nLoss l' (EnergyBalance {txOut, txIn}) = l' + (txOut - txIn)
+
+lastWait :: (IsStream t, Monad m) => Time.UTCTime -> t m EnergyState -> t m Time.NominalDiffTime
+lastWait t es = S.map snd $ S.scanl' sf (t, 0 :: Time.NominalDiffTime) es
+  where
+    sf :: (Time.UTCTime, Time.NominalDiffTime) -> EnergyState -> (Time.UTCTime, Time.NominalDiffTime)
+    sf (ptime, _) e = (utcTNow e, Time.diffUTCTime (utcTNow e) ptime)
+
+
+energyStream :: (IsStream t, Monad m) => Time.UTCTime -> t m EnergyState -> t m (Time.UTCTime, EnergyBalance)
+energyStream t es = S.scanl' energyAtT (t, mempty) es
+  where 
+    energyAtT :: (Time.UTCTime, EnergyBalance) -> EnergyState -> (Time.UTCTime, EnergyBalance)
+    energyAtT (prevT, prevEb) es' = (tNow, prevEb <> eb)
+      where
+        eb = EnergyBalance txIn' txOut' cnsm' gen'
+        txIn' :: WattSeconds
+        txIn' = integrate $ p batteryVoltage gridToBatteryCurrent 
+        txOut' :: WattSeconds
+        txOut' = integrate $ p batteryVoltage batteryToGridCurrent
+        cnsm' :: WattSeconds
+        cnsm' = integrate $ p batteryVoltage batteryToLoadCurrent
+        gen' :: WattSeconds
+        gen' = integrate $ p batteryVoltage solarInputCurrent
+        p v i = es' ^. v * es' ^. i
+        integrate p' = p' * delT
+        delT = realToFrac $ Time.diffUTCTime tNow prevT
+        tNow = utcTNow es'
+
+powerStream :: (IsStream t, (Monad m)) => t m EnergyState -> t m (Power Watts)
+powerStream = S.map powerAtT
+  where
+    powerAtT :: EnergyState -> Power Watts
+    powerAtT es = Power txIn' txOut' cnsm' gen'
+      where
+        txIn' = p batteryVoltage gridToBatteryCurrent 
+        txOut' = p batteryVoltage batteryToGridCurrent
+        cnsm' = p batteryVoltage batteryToLoadCurrent
+        gen' = p batteryVoltage solarInputCurrent
+        p v i = es ^. v * es ^. i
---
+++ test/NodeSpec.hs
@@ -0,0 +1,49 @@
+module NodeSpec (spec) where
+
+import Node
+import Test.Hspec
+import Test.Hspec.QuickCheck
+
+import Streamly.Prelude as S
+import Streamly
+
+import qualified Data.Time as Time
+
+
+import Import
+
+emptyStream :: (Monad m) => SerialT m EnergyState
+emptyStream = S.replicate 100 defaultES 
+
+emptyStream' :: (Monad m) => SerialT m (NodeId Int, EnergyState)
+emptyStream' = S.zipWith (,) (S.fromList $ Import.map NodeId [0,1..100]) emptyStream
+
+spec :: Spec
+spec = do
+  describe "This is how we use node streams" $ do
+    let
+      initTime = Time.UTCTime (Time.fromGregorian 10 10 2019) 12
+      es :: (Monad m) => SerialT m EnergyBalance
+      es = S.map snd $ energyStream initTime emptyStream
+      ps = S.all (\a-> a == mempty) $ powerStream emptyStream
+      zeroNM :: NodeS
+      zeroNM = NodeMetrics 0 0 0 0 mempty mempty defaultES
+      thisState :: (Monad m) => SerialT m NodeS
+      thisState = runNodeMonitor initTime (NodeId 1) emptyStream'
+      fs :: (Monad m) => m (Maybe NodeS)
+      fs = S.head thisState
+    e <- S.foldl (<>) mempty id es
+    p <- ps
+    strd <- S.sum (stored es)
+    l <- S.sum (loss es)
+    d <- S.sum (demand es)
+    f <- fs
+    it "zero energyStream" $ e `shouldBe` mempty 
+    it "zero powerStream" $ p `shouldBe` True
+    it "zero stored" $ strd `shouldBe` 0
+    it "zero loss" $ l `shouldBe` 0
+    it "zero demand" $ d `shouldBe` 0
+    it "run Node Monitor" $ f `shouldBe` (Just $ zeroNM)
+    --it "loss 2" $ (S.foldl (<>) initEA id es) `shouldBe` initEA -- \i -> plus2 i - 2 `shouldBe` i
+    --it "lastWait" $ (S.foldl (<>) initEA id es) `shouldBe` initEA -- \i -> plus2 i - 2 `shouldBe`i
+
