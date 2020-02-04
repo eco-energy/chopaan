5ccb8e1c0e793705aecc710212913d02093c5505
Faez Shakil <faez@ecoenergyfinance.org>
2020-02-04 21:11:00 +0500

    only essentials running

--- src/Run.hs
+++ src/Run.hs
@@ -4,12 +4,9 @@ module Run (run) where
 
 import UI (runTUI, mkUIChan)
 import Mqtt (runMqtt, defMQOpts)
-import Transactor (runTransactor)
-import StateMonitor (runStateMonitor)
 import Registry (getKibbutz, nodes, mkCallback, outQueue, inQueue, queueStream, printQueueStream)
 
 import Import
-import qualified Streamly.Prelude as S
 import Control.Concurrent (forkIO)
 
 
@@ -18,7 +15,8 @@ run = do
   kbtz <- liftIO $ getKibbutz thingTypeName
   uiChan <- liftIO mkUIChan
   _ <- liftIO $ forkIO $ runMqtt defMQOpts (outQueue kbtz) (nodes kbtz) (mkCallback kbtz uiChan)
-  _ <- liftIO $ forkIO $ printQueueStream . queueStream . inQueue $ kbtz
+  -- _ <- liftIO $ forkIO $ printQueueStream . queueStream . inQueue $ kbtz
+  -- _ <- liftIO $ (print . show) =<< (readBChan uiChan)
   liftIO $ runTUI kbtz uiChan
   where
     thingTypeName = "kibbutz-pilot-node"
