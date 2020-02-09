0f171d61f4573e7d44a718230124f0c84fb0e837
Faez Shakil <faez@ecoenergyfinance.org>
2020-02-09 16:47:27 +0500

    streaming at the top

--- src/Run.hs
+++ src/Run.hs
@@ -2,22 +2,36 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Run (run) where
 
-import UI (runTUI, mkUIChan, nodeStream)
+import UI (runTUI, mkUIChan)
 import Mqtt (runMqtt, defMQOpts)
 import Registry (getKibbutz, nodes, mkCallback, outQueue, inQueue, queueStream, printQueueStream)
-
+import Node (NodeS, runNodeMonitor)
 import Import
 import Control.Concurrent (forkIO)
+import Streamly
+import qualified Streamly.Prelude as S
+import qualified Data.Time as Time
 
 
 run :: RIO App ()
 run = do
   kbtz <- liftIO $ getKibbutz thingTypeName
-  uiChan <- liftIO mkUIChan
+  uiChan <- liftIO $ mkUIChan
+  initTime <- liftIO $ Time.getCurrentTime
   _ <- liftIO $ forkIO $ runMqtt defMQOpts (outQueue kbtz) (nodes kbtz) (mkCallback kbtz uiChan)
   -- _ <- liftIO $ forkIO $ printQueueStream . queueStream . inQueue $ kbtz
   -- _ <- liftIO $ (print . show) =<< (readBChan uiChan)
-  -- liftIO $ S.mapM nodeStream k
+  let
+    -- nmNow = runNodeMonitor initTime
+    ns' :: Parallel (NodeS)
+    ns' = parallely . adapt $ foldr (<>) (runNodeMonitor initTime n s) $ [(runNodeMonitor initTime n' s) | n' <- ns]
+      where
+        s = queueStream $ inQueue kbtz
+        (n:ns) = nodes kbtz
+  -- _ <- liftIO $
   liftIO $ runTUI kbtz uiChan
   where
     thingTypeName = "kibbutz-pilot-node"
+    updateState = undefined
+    toDB = undefined
+    
