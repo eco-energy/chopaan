cf02304dc519185f08c71f32e719970bcdc6e4d4
Faez Shakil <faez@ecoenergyfinance.org>
2020-02-27 21:05:00 +0500

    confirmed all stakes are converted into ETRs

--- test/TransactorSpec.hs
+++ test/TransactorSpec.hs
@@ -1,3 +1,36 @@
 module TransactorSpec where
 
 import Transactor
+
+import Test.Hspec
+import Test.QuickCheck.Classes
+import Test.QuickCheck.Checkers
+import Test.QuickCheck
+import Test.QuickCheck.Instances.Time ()
+
+import qualified Data.Time as Time
+import qualified Data.Text as Text
+import Data.ULID (getULID)
+import Registry (NodeT)
+import Node (NodeId(..))
+import Lens.Micro
+import Proto.NodeMessages as NM
+import Proto.NodeMessages_Fields as NM
+
+
+
+spec :: Spec
+spec = do
+  describe "The transactor converts stake forms and dispatches ETRs" $ do
+    it "selected stake number is the same as the generated etrs" $ do
+      t <- Time.getCurrentTime
+      uid <- getULID
+      let
+        d = (60 :: Time.NominalDiffTime)
+        ns :: [NodeT]
+        ns = map (\n-> NodeId (Text.pack $ show n)) [1..10::Int]
+        stks = map (\s -> s & participating .~ True & power .~ 30) $ map initStake ns 
+        etrs = prepTx stks uid t d
+      mapM_ (\(etr, stk)-> etr ^. powerInWatts `shouldBe` (abs $ _power stk)) $ zip ((map snd) . fst $ etrs) stks
+      (length (fst $ etrs)) `shouldBe` (length stks)
+      
