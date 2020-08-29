8955e138e080eedf75a0e09fc669be7c386c0dbe
Faez Shakil <faez@ecoenergyfinance.org>
2020-08-29 07:24:05 +0500

    cleanup

--- src/Chopaan/Run.hs
+++ src/Chopaan/Run.hs
@@ -4,28 +4,23 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Chopaan.Run (run) where
 
-import Chopaan.Node.Node (gridS, writeCSVRecords, NodeS)
+import Chopaan.Node.Node (NodeS)
 import Chopaan.Types
 import RIO
 import Control.Concurrent (forkIO)
 
 import Streamly
-import qualified Streamly.Prelude as S
-
-import Control.Monad.State.Lazy (runStateT)
-
-import RIO.Time
-
 import Chopaan.Comm.Mqtt (runMqtt)
-import Chopaan.Comm.Comm (MessageQs(..), initQs, mkCallback, subStream)
+import Chopaan.Comm.Comm (MessageQs(..), initQs, mkCallback)
+
 import Chopaan.Kibbutz.Kibbutz (Kbtz(..), sensorKbtz, logsKbtz, rsKbtz, getNodes, asFRPNetwork)
+
 import Chopaan.UI.Monitor (monitor, MonitorC)
-import Chopaan.Node.NodeId
+import Chopaan.Node.NodeId (NodeMAC)
 import Proto.NodeMessageSchema.NodeMessages
-import Reflex.Vty (mainWidget, VtyWidget, runVtyApp)
+import Reflex.Vty (mainWidget, VtyWidget)
 import Reflex
-import Reflex.Spider
-import Control.Monad.Trans
+
 
 run :: RIO App ()
 run = do
@@ -40,9 +35,7 @@ run = do
   logs    <- liftIO $ logsKbtz @SerialT nodes logsQ
   _ <- liftIO $ forkIO $ forever $
        runMqtt mqttOpts outbox nodes (mkCallback qs)
-  liftIO $ mainWidget $ mon sensors runtime logs 
-  where
-    writer stateMap = runStateT (writeCSVRecords "test.csv" stateMap) (UTCTime (fromGregorian 1 1 2020) 0)
+  liftIO $ mainWidget $ mon sensors runtime logs
 
 
 mon :: forall m m' t' t a. (TriggerEvent t' m', MonitorC t' m', MonadAsync m, IsStream t, Show a)
