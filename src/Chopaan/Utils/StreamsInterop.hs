761575a614b9191832c00882d3ac88722ed8968d
Faez Shakil <faez@ecoenergyfinance.org>
2020-09-02 02:50:46 +0500

    reflex -> streamly added

--- src/Chopaan/Utils/StreamsInterop.hs
+++ src/Chopaan/Utils/StreamsInterop.hs
@@ -1,21 +1,37 @@
 {-# LANGUAGE RankNTypes, TypeApplications, ExplicitForAll, ScopedTypeVariables #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GADTs, ConstraintKinds #-}
 module Chopaan.Utils.StreamsInterop where
 
 import Streamly
 import qualified Streamly.Prelude as S
 import qualified Reflex as R
 
-import Control.Monad (void, liftM)
+import Control.Monad (void)
 import Control.Monad.IO.Class (liftIO, MonadIO)
 import Streamly.Internal.Data.Stream.StreamK (hoist)
+import Control.Concurrent (forkIO)
 
+
+type StoRConstraints t t' m m' = (IsStream t, MonadAsync m, R.Reflex t', R.TriggerEvent t' m', MonadIO m')
+
+type RtoSConstraints t t' m = (IsStream t, MonadAsync m, R.Reflex t', R.MonadHold t' m)
 
 -- | streamly to event
-toEvent :: forall t t' m m'. (IsStream t, MonadAsync m, R.Reflex t', R.TriggerEvent t' m', MonadIO m')
+toEvent :: forall t t' m m'. (StoRConstraints t t' m m')
         => (forall a. m a -> IO a) -> (forall a. Show a => t m a -> m' (R.Event t' a))
 toEvent h s = do
   (e, fire) <- R.newTriggerEvent
-  liftIO . S.drain $ S.mapM (void . fire) $ (hoist h) . adapt $ s
+  _ <- liftIO . forkIO . S.drain $ S.mapM (void . fire) $ (hoist h) . adapt $ s
   return e
+
+fromEvent :: forall t t' m. (RtoSConstraints t t' m) => (forall a. R.Event t' a -> t m a) 
+fromEvent e = S.unfoldrM iterOverEvent e
+  where
+    iterOverEvent :: R.Event t' a -> m (Maybe (a, R.Event t' a))
+    iterOverEvent ex = do
+      b <- R.hold Nothing $ Just <$> ex
+      h <- R.sample b
+      case h of
+        Nothing -> return $ Nothing
+        Just he -> return $ Just (he, ex)
