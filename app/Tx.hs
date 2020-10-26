930f312917a268f3db8dcabcd5c89589a8b4b9f3
Faez Shakil <faez@ecoenergyfinance.org>
2020-10-26 18:04:03 +0500

    new exes

---
+++ app/Tx.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE OverloadedStrings, TypeApplications, NamedFieldPuns #-}
+module Main where
+
+import Dhall
+import Chopaan.Node.NodeId
+import Chopaan.Kibbutz.Transactor (Tx(..), Role(..), mkStake, dispatchTx)
+import Chopaan.Comm.Mqtt (pub, client)
+import Chopaan.Comm.Comm (initNodeQueue, trivialCB)
+import Chopaan.Types
+import qualified Data.Map.Strict as Map
+import Control.Concurrent.STM (atomically)
+import Control.Concurrent
+import Control.Monad
+import Proto.NodeMessageSchema.NodeMessages
+
+nodeId :: NodeMAC
+nodeId = NodeId "3c:71:bf:64:45:20"
+
+main = do
+  Options{mqttOpts} <- input auto "./options.dhall"
+  outbox <- atomically $ initNodeQueue @NodeMAC @(MeshFrame)
+  cl <- client mqttOpts trivialCB 
+  let txs = txTest 100 nodeId
+  forkIO $ forever $ (pub cl outbox)
+  mapM_ (\t -> dispatchTx outbox t >> (threadDelay $ 1000*1000) >> print ("Dispatched! ")) txs
+
+
+txTest :: Int -> NodeMAC -> [Tx NodeMAC]
+txTest n addr = [ testTx addr | _ <- enumFromTo 0 n ]
+
+
+
+testTx :: NodeMAC -> Tx NodeMAC
+testTx addr = Tx $ Map.fromList [(addr, mkStake Source 100 100)]
