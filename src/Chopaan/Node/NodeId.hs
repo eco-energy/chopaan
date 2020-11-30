d3c11ae42865183fee0c6f9ffe5671e8a23239ab
Faez Shakil <faez@ecoenergyfinance.org>
2020-11-30 15:23:02 +0500

    use generic to derive instance

--- src/Chopaan/Node/NodeId.hs
+++ src/Chopaan/Node/NodeId.hs
@@ -8,13 +8,11 @@ import qualified Data.Text as Text
 
 newtype NodeId a = NodeId { unNodeId :: a } deriving (Eq, Show, Ord, Generic)
 
-instance (Hashable a) => Hashable (NodeId a) where
-  hashWithSalt n (NodeId a) = hashWithSalt n a
+instance (Hashable a) => Hashable (NodeId a)
 
 instance (ToField a) => ToField (NodeId a) where
   toField (NodeId a) = toField a 
 
 type ThingName = Text.Text
-
 
 type NodeMAC = NodeId ThingName
