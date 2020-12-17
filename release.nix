ddd3b851ab88f7416c1fb34471a10ae661688285
Faez Shakil <faez@ecoenergyfinance.org>
2020-12-17 07:53:16 +0500

    this does not cross-compile yet

--- release.nix
+++ release.nix
@@ -1,16 +1,23 @@
 # release.nix
 let
-  chopaan = import(./default.nix);
+  sources = import ./nix/sources.nix;
+  haskellNix = import sources."haskell.nix" {};
+  nixpkgsSrc = haskellNix.sources.nixpkgs-2003;
+  nixpkgsArgs = haskellNix.nixpkgsArgs;
+  # import nixpkgs with overlays
+  pkgs = import nixpkgsSrc nixpkgsArgs;
 
-  pkgsNative = import <nixpkgs> {};
-  pkgsRaspberryPi = import <nixpkgs> {
+  chopaan = import(./default.nix);
+  nativeArgs = nixpkgsArgs // {
     crossSystem = pkgsNative.lib.systems.examples.raspberryPi;
   };
+  pkgsNative = pkgs;
+  pkgsRaspberryPi = import nixpkgsSrc nativeArgs; 
 
   native = chopaan { pkgs = pkgsNative; };
   crossRaspberryPi = chopaan { pkgs = pkgsRaspberryPi; };
 
 in {
-  chopaan-native = native.chopaan.components.exes.chopaan;
-  chopaan-raspberry-pi = crossRaspberryPi.chopaan.components.exes.chopaan;
+  chopaan-native = native.chopaan.components.exes.chopaan-exe;
+  chopaan-raspberry-pi = crossRaspberryPi.chopaan.components.exes.chopaan-exe;
 }
