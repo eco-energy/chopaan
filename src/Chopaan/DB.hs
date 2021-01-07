17a8d2d5fc3d94a2ff4ca272d7620c7bbc385149
Faez Shakil <faez@ecoenergyfinance.org>
2021-01-07 23:12:43 +0500

    added dbpool

--- src/Chopaan/DB.hs
+++ src/Chopaan/DB.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE Arrows #-}
 {-# LANGUAGE FlexibleContexts, FlexibleInstances, MultiParamTypeClasses, RecordWildCards, RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables, TypeApplications, TypeFamilies #-}
 
-module Chopaan.DB (Persisted(..), getDbConn) where
+module Chopaan.DB (Persisted(..), dbPool, DBPool) where
 
 import Opaleye (
   Field, Table(Table), Insert(..)
@@ -11,8 +12,11 @@ import Opaleye (
   , runInsertMany
   , SqlInt4, SqlFloat8, SqlText, SqlTimestamptz, toFields, sqlUTCTime
   )
-import Database.PostgreSQL.Simple (Connection, connect, ConnectInfo(..))
+import Database.PostgreSQL.Simple (Connection, connect, ConnectInfo(..), close)
+
+import Control.Monad
 import Control.Monad.IO.Class
+import Data.Pool
 
 import Control.Monad.Trans.Reader
 
@@ -23,14 +27,21 @@ import Chopaan.DB.Nodes (insertNode)
 import Chopaan.Types (DBOpts(..))
 import Proto.NodeMessageSchema.NodeMessages (EnergyState, HardwareConfig)
 
+import Data.Time
+import Data.Maybe
 import RIO.Text (unpack)
 
 import Streamly
 import qualified Streamly.Prelude as S
 
 
-getDbConn :: DBOpts -> IO Connection
-getDbConn DBOpts{..} = connect ConnectInfo
+type DBPool = Pool Connection
+
+dbPool :: DBOpts -> IO (DBPool)
+dbPool dbOpts = createPool (getDbConn dbOpts) close 2 60 10
+
+getDbConn :: forall m. (MonadIO m) => DBOpts -> m Connection
+getDbConn DBOpts{..} = liftIO $ connect ConnectInfo
   { connectHost = unpack host
   , connectPort = fromIntegral port
   , connectDatabase = unpack database
@@ -42,6 +53,9 @@ type NodeConn m a = ReaderT (NodeMAC, DBOpts) m a
 
 runInsert :: (MonadIO m) => DBOpts -> NodeMAC -> (Connection -> NodeMAC -> a -> IO ()) -> a -> m ()
 runInsert db n i a = (\f -> f a) =<< runReaderT (insertConn i)  (n, db)
+
+
+fromPool pool = withResource pool insertConn
 
 insertConn :: (MonadIO m) => (Connection -> NodeMAC -> a -> IO ()) -> ReaderT (NodeMAC, DBOpts) m (a -> m ())
 insertConn insert = do
@@ -57,10 +71,10 @@ insertConn insert = do
 insertES' :: MonadIO m => DBOpts -> NodeMAC -> EnergyState -> m ()
 insertES' dbOpts n = runInsert dbOpts n insertEnergyState
 
-readES :: (IsStream t, MonadAsync m) => DBOpts -> NodeMAC -> t m EnergyState
-readES db n = S.repeatM (liftIO (readES' n =<< getDbConn db))
+readES :: forall m. (MonadIO m) => DBOpts -> NodeMAC -> m EnergyState
+readES db n = (readES' n =<< getDbConn db)
   where
-    readES' :: NodeMAC -> Connection -> IO (EnergyState)
+    readES' :: NodeMAC -> Connection -> m (EnergyState)
     readES' = undefined -- selectTable sensorsTable
 
 readN :: (IsStream t, MonadAsync m) => DBOpts -> NodeMAC -> t m HardwareConfig
@@ -73,15 +87,55 @@ insertNode' :: MonadIO m => DBOpts -> NodeMAC -> HardwareConfig -> m ()
 insertNode' dbOpts n a = liftIO $ runInsert dbOpts n (\conn -> (\_ _ -> runReaderT (insertNode a) (conn, n))) =<< getDbConn dbOpts
 
 
+type Range = (LocalTime, LocalTime)
 
-class (IsStream t, MonadAsync m) => Persisted t m a where
-  save :: DBOpts -> NodeMAC -> t m a -> m ()
-  retrieve :: DBOpts -> NodeMAC -> t m a
+class HasPool p where
+  type Opts p
+  getPool :: (MonadAsync m) => Opts p -> m (Pool p)
 
+instance HasPool Connection where
+  type Opts Connection = DBOpts
+  getPool = liftIO . dbPool
+
+
+
+class (Show n, Show a) => Persisted n a where
+  save :: (HasPool p, MonadAsync m) => p -> n -> a -> m (Maybe n)
+  retrieve :: (HasPool p, MonadAsync m) => p -> n -> m (Maybe (n, a))
+
+class Key a where
+  hasKey :: a -> String
+
+saveStream :: forall p t m n a. (IsStream t, MonadAsync m, Key n, HasPool p, Persisted n a)
+  => Pool p
+  -> t m (n, a)
+  -> m ()
+saveStream pool s = withResource pool (S.drain . adapt . serialWrite)
+  where
+    serialWrite :: p -> t m (Maybe n)
+    serialWrite conn = S.mapM (c conn) s
+    c :: p -> (n, a) -> m (Maybe n)
+    c = \conn (n, a) -> (save conn n a)
+
+{--
+readStream :: forall p t m n a.
+  (IsStream t, MonadAsync m, Key n, HasPool p, Persisted n a)
+  => (Persisted n a)
+  => (Pool p) -> (n -> n) -> n -> m (t m (n, a))
+readStream pool nextN n r = do
+  let asStream con = (fmap fromJust . snd)
+        $ S.filter (isJust) readStream
+        where
+          readStream :: t m (Maybe (n, a))
+          readStream = S.iterateM (\prev ->
+                        retrieve con (nextN prev))
+                       (n, Nothing)
+  withResource pool asStream
+--}
+
+{--
 instance (IsStream t, MonadAsync m) => Persisted t m EnergyState where
   save dbOpts nodeMAC s = S.mapM_ (insertES' dbOpts nodeMAC) $ adapt s
   retrieve = readES 
+--}
 
-instance (IsStream t, MonadAsync m) => Persisted t m HardwareConfig where
-  save dbOpts nodeMAC s = S.mapM_ (insertNode' dbOpts nodeMAC) $ adapt s
-  retrieve = readN
