ae9388066f3c428346d22929d2263a0910ad4239
Faez Shakil <faez@ecoenergyfinance.org>
2021-01-15 07:49:45 +0500

    simple retry

--- src/Chopaan/Utils/Retry.hs
+++ src/Chopaan/Utils/Retry.hs
@@ -2,10 +2,16 @@ module Chopaan.Utils.Retry where
 
 import Control.Retry
 import Data.Either
+import Control.Monad.IO.Class
 
-retryEither :: n -> (n -> IO (Either a b)) -> IO (Either a b)
+retryEither :: (MonadIO m) => n -> (n -> m (Either a b)) -> m (Either a b)
 retryEither n f = retrying retryPolicyDefault shouldRetryEither (\retryStatus ->  f n)
 
 shouldRetryEither :: (Monad m) => RetryStatus -> (Either a b) -> m Bool
 shouldRetryEither _ = return . isLeft
 
+retryBool :: (MonadIO m) => m (Bool) -> m (Bool)
+retryBool action = retrying retryPolicyDefault (\_ a -> return $ a == True) (\retryStatus ->  do
+                                                                                x <- action
+                                                                                return x
+                                                                            )
