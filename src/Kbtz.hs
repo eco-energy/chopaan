cd2af2851aceda08f7b2add724210929456986fb
Faez Shakil <faez@ecoenergyfinance.org>
2021-02-20 01:56:43 +0500

    tagless final effects?

--- src/Kbtz.hs
+++ src/Kbtz.hs
@@ -1,23 +1,45 @@
-{-# LANGUAGE ExistentialQuantification, QuantifiedConstraints, FlexibleContexts, FlexibleInstances #-}
+{-# LANGUAGE ExistentialQuantification
+, QuantifiedConstraints
+, FlexibleContexts
+, FlexibleInstances
+, FunctionalDependencies
+#-}
 
 module Kbtz where
 
 import Chopaan.Types
-import Chopaan.Comm.Comm (Address, Dispatch, MessageQs, initMessageQs)
+import Chopaan.Comm.Comm (
+  Address(..)
+  , Dispatch
+  , MessageQs
+  , WriteChan
+  , PubQueue
+  , initMessageQs
+  , writeToPubQ)
+import Chopaan.Kibbutz.Kibbutz (sub, getNodes)
+import Chopaan.Node.NodeId
+import Chopaan.Kibbutz.KbtzId
 
 import Streamly
+import qualified Streamly.Prelude as S
+
 import RIO
 
-class (MonadAsync m) => KbtzM m where
-  getNodes :: forall n. (Address n) => m [n]
-  getQueues :: forall n. (Address n) => m (MessageQs n)
-  subscribe :: forall t n a. (IsStream t, Address n, Dispatch a)
-    => MessageQs n -> n -> t m a
-  publish :: forall t n a. (IsStream t, Address n, Dispatch a)
-    => MessageQs n -> t m (n, a) -> m ()
-  save :: forall n a. (Address n, Dispatch a) => (n, a) -> m ()
-  retrieve :: forall t n a. (IsStream t, Address n, Dispatch a) => n -> t m a
+class (MonadAsync m, Address n) => KbtzM m n | m -> n where
+  nodes :: KbtzName -> m [n]
+  getQueues :: m (MessageQs n)
+  subscribe :: forall t a. (IsStream t, Dispatch a)
+    => WriteChan n a -> n -> m (t m a)
+  publish :: forall t a. (IsStream t, Dispatch a)
+    => PubQueue -> t m (n, a) -> m ()
+  save :: forall a. (Dispatch a) => (n, a) -> m ()
+  retrieve :: forall t a. (IsStream t, Dispatch a) => n -> t m a
 
 
-instance KbtzM (ReaderT App IO) where
+instance KbtzM (ReaderT App IO) NodeMAC where
+  nodes = liftIO . runReaderT getNodes
   getQueues = liftIO initMessageQs
+  subscribe = sub
+  publish p s = S.mapM_ (\(n, a) -> liftIO $ writeToPubQ p (controlTopic n) a) $ adapt s
+  save = undefined
+  retrieve = undefined
