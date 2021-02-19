a892e891b9ccd37dd70f5106ff8414dc82f8eecc
Faez Shakil <faez@ecoenergyfinance.org>
2021-02-20 01:52:34 +0500

    we need a server abstaction that is separate from the UI

--- src/Chopaan/Run.hs
+++ src/Chopaan/Run.hs
@@ -7,13 +7,17 @@ module Chopaan.Run (run, mon) where
 
 import GHC.Generics
 import Chopaan.Types
-import RIO hiding (view, async)
+import RIO hiding (view, async, withAsync, Async)
 import qualified Data.Text as Text
 import Control.Concurrent (forkIO)
+import qualified Control.Concurrent.Async as A
 
 import Streamly
 import qualified Streamly.Prelude as S
 
+import Chopaan.Node.NodeId (NodeMAC)
+import Chopaan.Node.Folds (SensorS)
+import Proto.NodeMessageSchema.NodeMessages (RuntimeStats, HardwareConfig)
 import Chopaan.Comm.Mqtt (runMqtt)
 import Chopaan.Comm.Comm (MessageQs(..)
                          , initQs
@@ -22,6 +26,7 @@ import Chopaan.Comm.Comm (MessageQs(..)
                          , Address(..)
                          , PubQueue
                          )
+
 import Chopaan.Kibbutz.KbtzId
 import Chopaan.Kibbutz.Kibbutz ( sensorKbtz
                                , rsKbtz
@@ -31,14 +36,15 @@ import Chopaan.Kibbutz.Kibbutz ( sensorKbtz
                                )
 import Chopaan.Kibbutz.AWS.Things (withMqttAuth)
 import Chopaan.Kibbutz.AWS.Common (newLogger, LogLevel(..))
-import Chopaan.Kibbutz.Transactor (runTransactor, Tx(..), TransactionStatus, asKbtz)
+import Chopaan.Kibbutz.Transactor (runTransactor, Tx(..), TransactionStatus)
+import Chopaan.Kibbutz.Mesh
 
-import Chopaan.Ui (mon, defGrid)
 
+import Chopaan.Server (mon, defGrid)
+import Kbtz
 import Chopaan.DB
 import Chopaan.Utils.Retry
 import Chopaan.Testing (testNodes, testPub)
-
 
 
 data ChopaanState = ChopaanState
@@ -47,29 +53,43 @@ data ChopaanState = ChopaanState
   } deriving (Eq, Ord, Show, Generic)
 
 
+data KbtzSpec k n c = KbtzSpec { kbtzId :: k
+                               , kbtznodes :: [(n, HardwareConfig)]
+                             } deriving (Eq, Ord, Show, Generic)
+
 run :: RIO App ()
 run = do
   app <- ask
   let
     Options{..} = appOptions app
     KibbutzOpts{..} = kibbutzOpts
-    nodes = testNodes
+    --nodes = testNodes
   --nodes <- (runReaderT getNodes (KbtzId name))
-  dbpool <- liftIO . (recoverC 100) $ dbPool dbOpts
-  liftIO . print $ "DB Connection Pool Initialized"
-  liftIO . print =<< (liftIO . (recoverC 1) . getSchema $ dbOpts)
-  qs@MessageQs{..} <- liftIO $ initQs
+  --  kibbutzim = []
+  --dbpool <- liftIO . (recoverC 100) $ dbPool dbOpts
+  --liftIO . print $ "DB Connection Pool Initialized"
+  liftIO . print =<< (liftIO . (recoverC 500) . getSchema $ dbOpts)
+  --liftIO $ mapM (runKibbutz @AheadT @IO mqttOpts{connId=name}) kibbutzim
+  --liftIO $ print ("Running Monitor...")
+  --liftIO $ mon id (defGrid nodes) sensors runtime
+
+type Kibbutzim t m n a = (IsStream t, MonadAsync m) => Map KbtzName (Kbtz t m n a)
+
+
+
+
+
+runKibbutz :: forall t m. (KbtzM m NodeMAC) => MQTTOpts -> KbtzName -> m ()
+runKibbutz mqttOpts name = do
+  ns <- nodes name
   lg <- liftIO $ newLogger Debug stdout
-  inbox <- liftIO . forkIO $
-       withMqttAuth lg (KbtzId name)
-         (runMqtt mqttOpts{connId=name} outbox nodes (mkCallback qs))
-  outbox <- liftIO . forkIO $ testPub nodes outbox
-  sensors <- liftIO $ sensorKbtz @SerialT @IO nodes stateChan
-  runtime <- liftIO $ rsKbtz @SerialT @IO nodes statsChan
-  liftIO $ print ("Running Monitor...")
-  liftIO $ mon id (defGrid nodes) sensors runtime
-
-
-
-
-
+  qs' <-  (liftIO $ A.async (liftIO $ mqtt lg ns))
+  MessageQs{stateChan, statsChan, outbox} <- liftIO $ A.wait qs'
+  sensors <- sensorKbtz @AsyncT ns stateChan
+  runtime <- runKbtz <$> rsKbtz @AsyncT ns statsChan
+  let txStatus = runTransactor outbox (10 * 60) sensors
+  return ()
+  where
+    mqtt lg nodes = liftIO $
+       withMqttAuth lg name
+         (runMqtt mqttOpts nodes mkCallback)
