4a66ec952fab23a18ae02baf3363b01d8fe47d0f
Faez Shakil <faez@ecoenergyfinance.org>
2021-02-26 04:19:15 +0500

    fixed api. figure out decoding and copy publishing from transactor

--- src/Chopaan/Comm/Socket.hs
+++ src/Chopaan/Comm/Socket.hs
@@ -4,23 +4,73 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications, FlexibleContexts, ExplicitForAll #-}
 {-# LANGUAGE OverloadedStrings#-}
+{-# LANGUAGE ConstraintKinds #-}
 
-module Chopaan.Comm.Socket (sub, pub) where
+module Chopaan.Comm.Socket (subN, pubN) where
+
+import Data.Function ((&))
+
+import Control.Monad.Catch
+
+import Streamly
+import Streamly.Internal.Memory.Array.Types (Array)
+import qualified Streamly.Prelude as S
+import qualified Streamly.Network.Socket as SK
+import Streamly.Network.Socket (SockSpec(..))
+import Streamly.Internal.Network.Socket (handleWithM, handleWith)
+
+
+import Network.Socket
+import Unsafe.Coerce
+import Data.Word
 
 import Chopaan.Comm.Comm (Dispatch(..), Address(..))
 import Chopaan.Node.NodeId (NodeMAC)
 
-import Streamly
-import qualified Streamly.Prelude as P
+eth_p_all :: ProtocolNumber
+eth_p_all = 0x0003
 
 
-data SocketErr
+defSpec :: SockSpec
+defSpec = SockSpec
+       { sockFamily = AF_PACKET
+       , sockType = Raw
+       , sockProto = unsafeCoerce . getAddrInfo . unsafeCoerce $ eth_p_all 
+       , sockOpts = []
+       }
 
-sub :: forall t m a. (IsStream t, MonadAsync m) => NodeMAC -> m (t m a)
-sub = undefined
+defAddr = SockAddrUnix "what" --8090 (tupleToHostAddress (0,0,0,0))
 
-pub :: forall t m a. (IsStream t, MonadAsync m) => t m a -> t m (Maybe SocketErr)
-pub = undefined
+type ChannelConf = (SockSpec, SockAddr)
+
+type NodeConf = (NodeMAC, ChannelConf)
+
+type SocketTM t m = (IsStream t, MonadAsync m, MonadMask m)
+
+sub' :: forall t m. SocketTM t m
+  => ChannelConf -> t m (Array Word8)
+sub' (spec, addr) =
+  S.concatMap handleSocketRead (S.unfold SK.accept (maxListenQ, spec, addr))
+  where
+    handleSocketRead :: Socket -> t m (Array Word8)
+    handleSocketRead = flip handleWith (S.unfold SK.readChunks)
+    maxListenQ = 10
+
+
+decode :: (Address n, Dispatch a) => Array Word8 -> (n, a) 
+decode = undefined
+
+sub1 :: forall t m a. (SocketTM t m, Dispatch a) => NodeConf -> t m a
+sub1 (n, c) = fmap snd $ S.filter (\(n', _) -> n' == n) $ decode <$> (sub' c)
+
+pub1 :: forall t m n a. (SocketTM t m, Address n, Dispatch a) => n -> t m a -> t m (Maybe Bool)
+pub1 = undefined
+
+subN :: (SocketTM t m, Dispatch a) => [NodeConf] -> t m (t m a)
+subN = (fmap sub1) . S.fromList
+
+pubN :: forall t m n a. (SocketTM t m, Address n, Dispatch a) => [n] -> t m (t m a) -> t m (t m (Maybe Bool))
+pubN n xs = pub1 <$> S.fromList n <*> xs
 
 
 {--
