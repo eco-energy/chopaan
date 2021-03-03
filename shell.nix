f98839f18af7cd579ec147e43d6c40d76420e2c3
Faez Shakil <faez@ecoenergyfinance.org>
2021-03-03 21:25:15 +0500

    bump nixpkgs

--- shell.nix
+++ shell.nix
@@ -6,7 +6,7 @@
 # hence you will be more likely to get cache hits when using these.
 # But you can also just use your own, e.g. '<nixpkgs>'.
 
-, nixpkgsSrc ? haskellNix.sources.nixpkgs-2003
+, nixpkgsSrc ? haskellNix.sources.nixpkgs-2009
 
 # haskell.nix provides some arguments to be passed to nixpkgs, including some
 # patches and also the haskell.nix functionality itself as an overlay.
