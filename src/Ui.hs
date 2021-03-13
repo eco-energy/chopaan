4ad9b1806e2a0b6e0b8cfeeecdddfde1c979820a
Faez Shakil <faez@ecoenergyfinance.org>
2021-03-13 23:43:44 +0500

    Shpadoinkle Ui

--- src/Ui.hs
+++ src/Ui.hs
@@ -7,17 +7,13 @@
 module Ui where
 
 import ConCat.Misc (inNew, inNew2, (:*), (:+))
-import ConCat.Category ((&&&))
 import Control.PseudoInverseCategory (pimap, EndoIso(..), PseudoInverseCategory(..))
 import qualified Control.Newtype.Generics as N
 import Control.DeepSeq (NFData)
 import Data.Monoid (Sum(..))
-import Data.Maybe (fromMaybe)
 import Data.Text (Text, pack, unpack)
 import Data.Bifunctor
-import GHC.Generics (Generic)
-
-import Safe (readMay)
+import GHC.Generics (Generic, Generic1)
 
 import Shpadoinkle.Backend.ParDiff (runParDiff)
 
@@ -37,7 +33,8 @@ data Op = Op1 | Op2 | Op3 | Op4 | Op5
   deriving (Eq, Ord, Show, Read, Enum, Bounded, Generic, NFData)
 
 newtype AnOp = AnOp { unOp :: Op }
-  deriving (Eq, Ord, Show, Read, Bounded, Generic, NFData)
+  deriving (Eq, Ord, Show, Read, Bounded, Generic)
+  deriving newtype (NFData)
 
 instance Enum AnOp where
   toEnum = AnOp . toEnum
@@ -49,8 +46,11 @@ deriving newtype instance Integral (Sum Int)
 
 
 newtype S = S { unS :: Sum Int }
-  deriving stock (Eq, Ord, Show, Generic)
+  deriving stock (Eq, Ord, Generic)
   deriving newtype (Num, NFData, Semigroup, Monoid)
+
+instance Show S where
+  show = show . getS
 
 deriving newtype instance Enum S
 deriving newtype instance Real S
@@ -68,13 +68,15 @@ inS2 f = \s s' -> S . Sum . (uncurry f) $ (getS s, getS s')
 instance N.Newtype S
 
 
-deriving instance (Generic state, Generic flow) => Generic (Graph flow state)
-deriving instance (Generic state, Generic flow, NFData state, NFData flow) => NFData (Graph flow state)
-
-
 newtype Gr flow state = Gr { unGr :: (Graph flow state) }
-  deriving stock (Eq, Ord, Show, Generic)
+  deriving stock (Eq, Ord, Show, Generic, Generic1)
   deriving newtype (Num, NFData, Functor, Bifunctor)
+
+
+deriving instance (NFData state, NFData flow) => NFData (Graph flow state)
+deriving instance Generic (Graph flow state)
+deriving instance Generic1 (Graph flow)
+
 
 emptyGr :: (GrConn flow state) => Gr flow state
 emptyGr = Gr empty
@@ -83,20 +85,11 @@ grProps :: [(Text, Prop m (Gr flow state))]
 grProps = []
 
 grRow :: (Show a, Show b, Show c) => (a, b, c) -> Text
-grRow (l, e, e') = "S: "
-                   <> (pack . show $ e)
-                   <> " T: "
-                   <> (pack . show $ e')
-                   <> " L: " <> (pack . show $ l)
-
-
---instance Functor (Gr flow) where
---  fmap f (Gr r) = Gr (fmap f r)
+grRow (l, e, e') = (pack . show $ l)
 
 instance N.Newtype (Gr flow state)
 
 -- $ Shpadoinkle Instances
-
 instance (Show state, Show flow) => Humanize (Gr flow state)
 
 
@@ -110,7 +103,7 @@ instance (GrConn flow state) => T.Tabular (Gr flow state) where
     -> T.Row (Gr flow state) -> T.Column (Gr flow state) -> [Html m (Gr flow state)] 
   toCell (Gr g) (Row _) (Column col) =
     let --c = context ((==) col) gr
-        es = [(l, x, x') | (l, x, x') <- edgeList g, x == col]
+        es = edgeList g
         grHtml :: Monad m => [Html m (Gr flow state)]
         grHtml = xs <$> es
           where
@@ -120,10 +113,10 @@ instance (GrConn flow state) => T.Tabular (Gr flow state) where
             iso = EndoIso id isoL isoR
               where
                 isoL :: [(flow, state, state)] -> Gr flow state
-                isoL xs' = foldl (inNew2 overlay) (emptyGr) (fmap (Gr . e') xs')
+                isoL xs' = foldl ov emptyGr (fmap e' xs')
                 isoR :: Gr flow state -> [(flow, state, state)]
                 isoR = edgeList . unGr
-                e' (x, y, z) = edge x y z
+                e' (x, y, z) = Gr $ edge x y z
                 
     in grHtml  
   sortTable ::
@@ -134,7 +127,7 @@ instance (GrConn flow state) => T.Tabular (Gr flow state) where
     T.ASC -> compare a b
     T.DESC -> compare b a
 
--- Data Instance and Instances
+-- $ Data Instance and their Instances
 
 data instance T.Row (Gr flow state) = Row (flow, state, state)
 data instance T.Column (Gr flow state) = Column state
@@ -167,6 +160,14 @@ type GrConn f s = (Bounded s, Show s, Ord s, Eq s, Enum s, Show f, Monoid f, Ord
 -- $ Constraints for edge labels and nodes, along with monad constraints
 type GrConnM m f s = (Monad m, GrConn f s)
 
+-- $ Overlay two Gr
+ov :: (GrConn f s) => Gr f s -> Gr f s -> Gr f s
+ov = inNew2 overlay
+
+-- Fold over a collection of graphs to get a graph
+mergeGr :: (GrConn f s, Foldable t) => t (Gr f s) -> Gr f s 
+mergeGr = foldl ov emptyGr
+
 -- $ render a graph to html
 grView :: forall m f s. (GrConnM m f s)
   => Gr f s
@@ -175,39 +176,10 @@ grView :: forall m f s. (GrConnM m f s)
 grView = T.view @m
 
 data Model l n = Model
-  { operation :: n
-  , nomber :: Int
-  , graph :: Gr l n
-  }
+  { graph :: Gr l n }
   deriving (Eq, Show, Generic)
 deriving instance (Generic s, Generic o, NFData s, NFData o) => NFData (Model s o)
 
-opFunction :: Op -> Int -> Int
-opFunction op _ = fromEnum op
-
-opText :: Op -> Text
-opText = pack . show
-
-opSelect :: Html m Op
-opSelect = select [ onOption $ const . read . unpack ]
-  $ opOption <$> [minBound, maxBound]
-  where opOption o = option [ value . pack $ show o ] [ text $ opText o ]
-
-num :: Int -> Html m Int
-num x = input'
-  [ value . pack $ show x
-  , onInput $ const . fromMaybe 0 . readMay . unpack
-  ]
-
-
-
--- $ The view takes continuations are arguments
-view :: (Monad m) => Model S Op -> Html m (Model S Op)
-view model = div_
-  [ liftC (\l m -> m {nomber = l}) nomber $ num $ nomber model
-  , text $ " = " <> pack (show $ opFunction (operation $ model) (nomber model))
-  , liftC (\g' m -> m {graph = g'}) graph $ pimap isoSort' (grView (graph model) (sortCol model))
-  ]
 
 isoSort :: forall f s. (GrConn f s) => EndoIso (Gr f s) (Gr f s, T.SortCol (Gr f s))
 isoSort = EndoIso id (\a -> (a, sortColGr a)) fst
@@ -215,23 +187,27 @@ isoSort = EndoIso id (\a -> (a, sortColGr a)) fst
 isoSort' :: forall f s. (GrConn f s) => EndoIso (Gr f s, T.SortCol (Gr f s)) (Gr f s) 
 isoSort' = piinverse isoSort
 
+-- $ The view takes continuations as arguments
+
+view :: (Monad m) => Model S Op -> Html m (Model S Op)
+view model = div_
+  [ liftC (\g' m -> m {graph = g'}) graph $ pimap isoSort' (grView (graph model) (sortCol model)) ]
+
+
 -- $ TODO: head here after vertex list doesn't make any sense.
 -- $       should the column be a list?
 -- $       I can make it a subgraph
 sortColGr :: (GrConn f s) => Gr f s -> T.SortCol (Gr f s)
 sortColGr = (flip T.SortCol T.ASC) . (Column . head . vertexList . unGr) 
+
 sortCol :: (GrConn f s) => Model f s -> T.SortCol (Gr f s)
 sortCol = sortColGr . graph 
 
 main :: IO ()
 main = runJSorWarp 8080 $
-  simple runParDiff (Model (Op1) 0 (mergeGr $ take 100 $ (fmap snd) (iterate opS (0, emptyGr)))) view getBody
+  simple runParDiff (Model (mergeGr $ (fmap snd) (iterate opS (0, emptyGr)))) view getBody
 
-ov :: (GrConn f s) => Gr f s -> Gr f s -> Gr f s
-ov = inNew2 overlay
 
-mergeGr :: (GrConn f s, Foldable t) => t (Gr f s) -> Gr f s 
-mergeGr = foldl ov emptyGr
 
 
 gr :: f -> s -> s -> Gr f s
