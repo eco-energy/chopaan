3588dd967be185532efe3e233a52ea9713a7b56b
Faez Shakil <faez@ecoenergyfinance.org>
2021-03-18 00:54:13 +0500

    moved running to Kibbutzim

--- src/Chopaan/Run.hs
+++ src/Chopaan/Run.hs
@@ -8,29 +8,8 @@ module Chopaan.Run (run, mon) where
 import GHC.Generics
 import Chopaan.Types
 import RIO hiding (view, async, withAsync, Async)
-import qualified Control.Concurrent.Async as A
 
-import Streamly
-import qualified Streamly.Prelude as S
-
-import Chopaan.Node.NodeId (NodeMAC)
-import Chopaan.Node.Folds (SensorS)
-import Proto.NodeMessageSchema.NodeMessages (RuntimeStats, HardwareConfig)
-import Chopaan.Comm.Mqtt (runMqtt)
-import Chopaan.Comm.Comm (MessageQs(..)
-                         , mkCallback
-                         )
-
-import Chopaan.Kibbutz.KbtzId
-import Chopaan.Kibbutz.Kibbutz ( sensorKbtz
-                               , rsKbtz
-                               , runKbtz
-                               )
-import Chopaan.Kibbutz.AWS.Things (withMqttAuth)
-import Chopaan.Kibbutz.AWS.Common (newLogger, LogLevel(..))
-import Chopaan.Kibbutz.Transactor (runTransactor, TransactionStatus, TxPlan)
-
-
+import Chopaan.Kibbutz.Kibbutzim
 
 import Chopaan.Server (mon)
 import Kbtz
@@ -45,9 +24,9 @@ data ChopaanState = ChopaanState
   } deriving (Eq, Ord, Show, Generic)
 
 
-data KbtzSpec k n c = KbtzSpec { kbtzId :: k
-                               , kbtznodes :: [(n, HardwareConfig)]
-                             } deriving (Eq, Ord, Show, Generic)
+--data KbtzSpec k n c = KbtzSpec { kbtzId :: k
+--                               , kbtznodes :: [(n, HardwareConfig)]
+--                             } deriving (Eq, Ord, Show, Generic)
 
 run :: RIO App ()
 run = do
@@ -57,7 +36,7 @@ run = do
     KibbutzOpts{..} = kibbutzOpts
     --nodes = testNodes
   --nodes <- (runReaderT getNodes (KbtzId name))
-  --  kibbutzim = []
+  --kibbutzim = []
   --dbpool <- liftIO . (recoverC 100) $ dbPool dbOpts
   --liftIO . print $ "DB Connection Pool Initialized"
   liftIO . print =<< (liftIO . (recoverC 500) . getSchema $ dbOpts)
@@ -66,30 +45,3 @@ run = do
   --liftIO $ mon id (defGrid nodes) sensors runtime
 
 
-newtype Kibbutzim t m n = Kibbutzim {
-  unKibbutzim :: Map KbtzName (KbtzState t m n)
-  }
-
-
-
-data KbtzState t m n = KbtzState
-  { kSensors :: t m (n, SensorS)
-  , kRuntime :: t m (n, RuntimeStats)
-  , kTx :: t m (TxPlan n, t m TransactionStatus)
-  }
-
-
-runKibbutz :: forall m. (KbtzM m NodeMAC) => MQTTOpts -> KbtzName -> m ()
-runKibbutz mqttOpts name = do
-  ns <- nodes name
-  lg <- liftIO $ newLogger Debug stdout
-  qs' <-  (liftIO $ A.async (liftIO $ mqtt lg ns))
-  MessageQs{stateChan, statsChan, outbox} <- liftIO $ A.wait qs'
-  sensors <- sensorKbtz @AsyncT ns stateChan
-  runtime <- runKbtz <$> rsKbtz @AsyncT ns statsChan
-  let txStatus = runTransactor outbox (10 * 60) sensors
-  return ()
-  where
-    mqtt lg nodes = liftIO $
-       withMqttAuth lg name
-         (runMqtt mqttOpts nodes mkCallback)
