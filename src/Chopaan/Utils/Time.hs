0faf70324d757a8967ddb5d6e5a0c1bbd2610600
Faez Shakil <faez@ecoenergyfinance.org>
2021-05-08 12:43:30 +0500

    time read util for s3

--- src/Chopaan/Utils/Time.hs
+++ src/Chopaan/Utils/Time.hs
@@ -5,7 +5,7 @@ import Data.Time.Compat
 import Data.Time.LocalTime.Compat
 import Data.Time.Clock.POSIX.Compat
 import Data.Word
-
+import qualified Data.Text as T
 
 fromPico :: Pico -> Integer
 fromPico (MkFixed i) = i
@@ -17,12 +17,16 @@ asUTC = localTimeToUTC tz
 
 timeToUIntSeconds :: LocalTime -> Word64
 timeToUIntSeconds = fromInteger . (\x -> round $ (realToFrac x) / 10e11) . fromPico . nominalDiffTimeToSeconds . utcTimeToPOSIXSeconds . asUTC
-    
 
 
 -- $ converts the millisecond timestamp in the EnergyState to a UTCTime  
 utcTimeNow :: Word64 -> UTCTime
 utcTimeNow = posixSecondsToUTCTime . fromIntegral
+
+-- $ converts the millisecond timestamp in the EnergyState to a UTCTime  
+parseUTCTime :: T.Text -> UTCTime
+parseUTCTime = utcTimeNow . read . T.unpack
+
 
 diffUTC :: UTCTime -> UTCTime -> DiffTime
 diffUTC a b = realToFrac $ diffUTCTime a b
