f353bdd3586b1bd4202d703f2253c865e1c84d90
Faez Shakil <faez@ecoenergyfinance.org>
2021-06-15 23:30:47 +0500

    all arbitrary are live here

--- test/Common.hs
+++ test/Common.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveGeneric, GeneralizedNewtypeDeriving, DerivingStrategies, StandaloneDeriving, TypeApplications, TypeSynonymInstances, FlexibleInstances, ScopedTypeVariables, OverloadedStrings, FlexibleContexts #-}
 module Common (
   module Test.Hspec,
   module Test.QuickCheck.Checkers,
@@ -7,8 +8,110 @@ module Common (
 import Test.Hspec
 import Test.QuickCheck.Checkers
 import Test.QuickCheck
+import Test.QuickCheck.Arbitrary.Generic
+import Test.QuickCheck.Instances.Time
+import Data.ProtoLens.Arbitrary
 
+import qualified Data.Text as T
+import qualified Data.Time as Ti
+import Numeric.Compensated
+
+import Chopaan.Graph
+import Chopaan.Node.NodeId
+import Chopaan.Node.Metrics
+import Chopaan.Node.Mesh
+import Chopaan.Kibbutz.Transactor
+import qualified Chopaan.Node.HW as HW
+import qualified Chopaan.Node.Components as C
+import Chopaan.Kibbutz.KbtzId (KbtzId(..))
+import Chopaan.Kibbutz
+import Chopaan.Graph.Spider
+
+import NetSpider.Snapshot
+import qualified NetSpider.Timestamp as NT
+
+import qualified Proto.NodeMessageSchema.NodeMessages as NM
+import qualified Proto.NodeMessageSchema.NodeMessages_Fields as NM
 
 almostEqual :: (Show a, Eq a, Num a, Ord a) => a -> a -> a -> Expectation
 almostEqual eta a b = do
   ((abs $ a - b) < eta) `shouldBe` True
+
+
+instance (Num a, Arbitrary a, Compensable a) => (Arbitrary (Compensated a)) where
+  arbitrary =  (\a -> pure $ add a 0 compensated) =<< arbitrary
+
+instance Arbitrary Watts where
+  arbitrary = genericArbitrary
+
+instance Arbitrary WattSeconds where
+  arbitrary = genericArbitrary
+
+instance (Arbitrary v) => Arbitrary (Node v) where
+  arbitrary = genericArbitrary
+
+
+instance (Arbitrary e, Arbitrary p) => Arbitrary (Battery e p) where
+  arbitrary = genericArbitrary
+
+
+instance (Arbitrary e, Arbitrary p) => Arbitrary (SensorMetrics e p) where
+  arbitrary = genericArbitrary
+
+instance Arbitrary (NodeMAC) where
+  arbitrary = do
+    let el = ['a'..'z']
+    xs <- mapM (\_ -> elements el) [1..6]
+    ys <- mapM (\_ -> elements el) [1..6]
+    let cpld = fmap (\(a, b) -> [a] <> [b]) $ zip xs ys
+    return $ NodeId . T.pack . tail $ foldl (\x y -> x <> ":" <> y) "" cpld
+
+instance Arbitrary (NM.EnergyState) where
+  arbitrary = arbitraryMessage
+
+instance Arbitrary (NM.RuntimeStats) where
+  arbitrary = arbitraryMessage -- do
+    -- mFH <- arbitrary @Word32
+    -- cFH <- arbitrary @Word32
+    -- cu <-  arbitrary @Word32
+    -- r <- arbitrary @Bool
+    -- w <- arbitrary @Int
+    -- ps <- arbitrary @Int
+    -- u <- arbitrary @Word64
+    -- return $ defMessage
+    --   & (NM.minFreeHeap .~ mFH)
+    --   & (NM.currentFreeHeap .~ cFH)
+    --   & (NM.cpuUtilization .~ cu)
+    --   & (NM.isRoot .~ r)
+
+
+
+instance Arbitrary NT.Timestamp where
+  arbitrary = genericArbitrary
+
+instance (Arbitrary n, Arbitrary v) => Arbitrary (SnapshotNode n v) where
+  arbitrary = genericArbitrary
+
+instance (Arbitrary n, Arbitrary l) => Arbitrary (SnapshotLink n l) where
+  arbitrary = genericArbitrary
+
+instance Arbitrary NodeVersion where
+  arbitrary = (pure . NodeVersion . T.pack) =<< arbitrary
+
+instance Arbitrary MeshNode where
+  arbitrary = genericArbitrary
+
+instance Arbitrary RxSignal where
+  arbitrary = genericArbitrary
+
+instance Arbitrary Role where
+  arbitrary = genericArbitrary
+
+instance Arbitrary TransactionStatus where
+  arbitrary = genericArbitrary
+
+instance Arbitrary Stake where
+  arbitrary = genericArbitrary
+
+instance (Arbitrary n) => Arbitrary (SG n) where
+  arbitrary = genericArbitrary
