01d95b88c8cb6ec1d20c69fc2c97f06c0ffa8487
Faez Shakil <faez@ecoenergyfinance.org>
2021-06-28 09:29:37 +0500

    depend on alpine

--- nix/docker.nix
+++ nix/docker.nix
@@ -19,19 +19,25 @@
 
 let
   exes = c.chopaan;
+  alp = pkgs.dockerTools.pullImage {
+      imageName = "alpine";
+      imageDigest = "sha256:52a197664c8ed0b4be6d3b8372f1d21f3204822ba432583644c9ce07f7d6448f";
+      sha256 = "sha256:1n589h9sg4lpxi0bmbh4ba682w6551jicx3zm5qph2lc0p2c5rb3";
+    };
   #ui = undefined
 in
 {
   server = pkgs.dockerTools.buildImage {
     name = "chopaan-server";
     tag = "latest";
+    fromImage = alp;
     contents = [ exes.server pkgs.iana-etc pkgs.cacert ];
     created = "now";
     config = {
       Cmd = [ "${exes.server}/bin/server" ];
       Version = "1.0";
       ExposedPorts = {
-        "8883/tcp" = {};
+        "8888/tcp" = {};
       };
     };
   };
@@ -39,13 +45,14 @@ in
   ui = pkgs.dockerTools.buildImage {
     name = "chopaan-ui";
     tag = "latest";
+    fromImage = alp;
     contents = [ exes.ui pkgs.iana-etc pkgs.cacert ];
     created = "now";
     config = {
       Cmd = [ "${exes.ui}/bin/ui" ];
       Version = "1.0";
       ExposedPorts = {
-        "8884/tcp" = {};
+        "8080/tcp" = {};
       };
     };
   };
@@ -53,13 +60,14 @@ in
   kbtzim = pkgs.dockerTools.buildImage {
     name = "chopaan-kbtzim";
     tag = "latest";
+    fromImage = alp;
     contents = [ exes.kbtzim pkgs.iana-etc pkgs.cacert ];
     created = "now";
     config = {
       Cmd = [ "${exes.kbtzim}/bin/kbtzim" ];
       Version = "1.0";
       ExposedPorts = {
-        "8885/tcp" = {};
+        "8883/tcp" = {};
       };
     };
   };
