0d675166103dc8d39d863232a51b23ec92a807a2
Faez Shakil <faez@ecoenergyfinance.org>
2021-06-28 09:25:21 +0500

    aesthetic

--- src/Chopaan/Graph/Spider.hs
+++ src/Chopaan/Graph/Spider.hs
@@ -251,7 +251,8 @@ hasConfig (h, p) label = defConfig
 {-# INLINE hasConfig #-}
 
 spiderPool :: forall m n v e. MonadIO m => Config n v e -> m (Pool (Spider n v e))
-spiderPool c = liftIO $ createPool ((recoverC "retrying kbtz janusgraph connection" 100) connectWith c) close 10 100 10
+spiderPool c = liftIO $ createPool
+  ((recoverC "retrying kbtz janusgraph connection" 100) (connectWith c)) close 10 100 10
 
 
 gridSnapshotSimple :: forall m n v e. (MonadIO m, SpiderConn n v e)
