387596394a98f5ad8547a557739607f68067a700
Faez Shakil <faez@ecoenergyfinance.org>
2021-06-29 13:36:02 +0500

    cleanup

--- deployment.nix
+++ deployment.nix
@@ -6,6 +6,7 @@ let
 in
 {
   network.description = "Chopaan and DB.";
+
   
   machine = { config, pkgs, resources, lib, ... }: {
       deployment = {
@@ -27,35 +28,11 @@ in
           ];
         };
       };
-      #fileSystems."/" =
-      #  { autoFormat = true;
-      #    fsType = "btrfs";
-      #    device = "/dev/nvme1n1";
-      #    ec2.size = 10;
-      #    ec2.volumeType = "gp2";
-      #  };
 
       boot.loader.grub.device = lib.mkForce "/dev/nvme0n1";
 
       networking.firewall.allowedTCPPorts = [ 80 8093 ];
 
-      # services.postgresql = {
-      #   enable = true;
-      #   extraPlugins = [ pkgs.timescaledb ];
-      #   settings = { shared_preload_libraries = "timescaledb"; };
-      #   authentication = ''
-      #     local all all ident map=mapping
-      #   '';
-
-      #   identMap = ''
-      #     mapping root     postgres
-      #     mapping postgres postgres
-      #   '';
-
-      #   package = pkgs.postgresql_11;
-
-      #   initialScript = ./db_migrations/1.psql;
-      # };
       docker-containers."janusgraph" = {
            image = "docker.io/janusgraph/janusgraph:latest";
            ports = [ "8182:8182" ];
