a8f6d6c8ce2bcc4c2019ce4d7e7bec8e912f7c5f
Faez Shakil <faez@ecoenergyfinance.org>
2021-07-05 19:50:00 +0500

    use main

--- app/Server.hs
+++ app/Server.hs
@@ -1,11 +1,8 @@
 module Main where
 
-import Network.Wai.Handler.Warp
-import Network.Wai.Middleware.Cors
-
-import qualified Chopaan.API.History as S
+import qualified Chopaan.Server as S
 
 
 
 main :: IO ()
-main = run 8888 $ (simpleCors $ S.historyApp "localhost" 8182)
+main = S.main
