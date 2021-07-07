02aa05b7c8995e63911839c4f4f59866e2aaf1e9
Faez Shakil <faez@ecoenergyfinance.org>
2021-07-07 14:50:36 +0500

    static n all yall

---
+++ nix/website.nix
@@ -0,0 +1,31 @@
+{ chan ? "5272327b81ed355bbed5659b8d303cf2979b6953"
+, system ? builtins.currentSystem
+, optimize ? true
+}:
+
+let
+  artifact = (import ./snowman.nix).build { isJS = true; };
+  pkgs = fetchFromGitHub = (import (builtins.fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/${c}.tar.gz";
+  }) { inherit system; });
+  file = if optimize then "all.min.js" else "all.js"
+  jslib = ../js;
+in
+pkgs.runCommand "website" {
+  LANG = "C.UTF-8"
+} ''
+  mkdir $out
+  mkdir $out/assets
+  mkdir $out/js
+  for src in ${jslib}/*
+  do
+    cp $src $out/js/$(basename $src)
+  done
+  for asset in ${assets}/*
+  do
+    cp $asset $out/assets/$(basename $asset)
+  done
+  cp ${artifact}/bin/ui.jsexe/${file} $out/all.min.js
+  cp ${artifact}/bin/ui.jsexe/index.html $out/index.html
+  echo ${pkgs.lib.commitIdFromGitRepo ../.git} > $out/version
+  ''
