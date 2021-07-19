20ae4b7ccd29e2e626938e69beb484ec8646db79
Faez Shakil <faez@ecoenergyfinance.org>
2021-07-19 13:48:23 +0500

    for incremental js

---
+++ linkDevAssets.sh
@@ -0,0 +1,33 @@
+OUT="./webdev"
+ARTIFACT="./dist-ghcjs/build/x86_64-linux/ghcjs-8.6.0.1/chopaan-0.1.0.0/x/ui/build/ui/ui.jsexe"
+
+jslib="./js"
+assets="./assets"
+
+if [ -d $OUT ]; then
+    echo "Deleting ${OUT}..."
+    rm -r $OUT
+fi
+
+echo "Recreating ${OUT}"
+mkdir -p $OUT
+mkdir -p $OUT/assets
+mkdir -p $OUT/js
+
+echo "Linking JS sources"
+for src in ${jslib}/*
+do
+    ln -s $src $OUT/js/$(basename $src)
+done
+
+echo "Linking Artifacts"
+for asset in ${assets}/*
+do
+    ln -s $asset $OUT/assets/$(basename $asset)
+done
+
+ln -s ${ARTIFACT}/all.js $OUT/all.min.js
+ln -s ${ARTIFACT}/index.html $OUT/index.html
+echo $(git log --pretty=format:'%h' -n 1) > $OUT/version
+
+echo "Asset Linking Finished!"
