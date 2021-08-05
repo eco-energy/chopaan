cdf4c057609cee6bddb7a745f27f283b24fff80e
Faez Shakil <faez@ecoenergyfinance.org>
2021-08-05 21:11:58 +0500

    timeline

---
+++ src/Chopaan/Ui/Timeline.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE OverloadedStrings, ExtendedDefaultRules, TypeApplications #-}
+module Chopaan.Ui.Timeline where
+
+import           Shpadoinkle                       (Html, MonadJSM, text, voidC)
+import qualified Shpadoinkle.Html                  as H
+import qualified Shpadoinkle.Html.Utils            as H
+import           Shpadoinkle.Lens
+
+import Control.Lens hiding (simple)
+
+import Data.Maybe
+import qualified Data.Vector as V
+import qualified Data.Text as T
+import qualified Data.Time as Ti
+
+import Chopaan.Utils.Time (dayRange)
+import qualified Chopaan.Ui.Style as Css
+
+import Shpadoinkle.Run (simple, runJSorWarp)
+import Shpadoinkle.Backend.Snabbdom (runSnabbdom, stage)
+
+default(T.Text)
+
+timeline :: (Functor m)
+  => (Ti.UTCTime, Ti.UTCTime)
+  -> Html m (Ti.UTCTime, Ti.UTCTime)
+timeline (start, end) = H.div sliderStack
+  [ H.div labelledSlider
+    [ H.label [] [H.text $ "Start Date :: "]
+    , generalize _1 $ rangeInput [H.onInput fromSlider] (0 :: Int) (length r)
+    ]
+  , H.div labelledSlider
+    [ H.label [] [H.text $ "End Date :: "]
+    , generalize _2 $ rangeInput [H.onInput fromSlider] (0 :: Int) (length r)
+    ]
+  ]
+  where
+    sliderStack = [H.class' $ Css.grid <> Css.grid_flow_col <> Css.grid_cols_2]
+    labelledSlider = [H.class' $ Css.grid <> Css.grid_flow_row <> Css.grid_rows_2]
+    fromSlider :: T.Text -> Ti.UTCTime -> Ti.UTCTime
+    fromSlider v t = fromMaybe t (getTime v)
+      where
+        getTime = (r V.!?) . ((flip div 10). round . read @Float . T.unpack)
+    r = V.fromList $ dayRange start end
+
+rangeInput props min max = H.input (props <> [ tp "type" "range"
+                                             , tp "min" (T.pack . show $ (min * 10))
+                                             , tp "max" (T.pack . show $ (max * 10))
+                                             , tp "step" (T.pack . show $ (10 :: Int))
+                                             ]) []
+
+
+tp = H.textProperty
+
+
+-- main = runJSorWarp 8080 $ do
+--   simple runSnabbdom (s, s) (timeline (s, e)) stage
+--   where
+--     s = Ti.UTCTime (Ti.fromGregorian 2021 8 4) 0
+--     e = Ti.UTCTime (Ti.fromGregorian 2022 8 4) 0
