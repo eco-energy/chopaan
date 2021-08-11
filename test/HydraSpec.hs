7c7d0bb1461e75c1f48bdddae840d4a2f160cad6
Faez Shakil <faez@ecoenergyfinance.org>
2021-08-11 07:36:09 +0500

    meh

---
+++ test/HydraSpec.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE PackageImports #-}
+module HydraSpec where
+
+import qualified Streamly.Prelude as S
+import Streamly.Internal.Data.Time.Units (MilliSecond64(..))
+import Test.Hspec
+import Test.QuickCheck.Checkers
+import Test.QuickCheck
+import Test.QuickCheck.Classes
+import Test.QuickCheck.Instances.Time
+
+import Control.Monad.IO.Class
+import qualified Data.Text as T
+import Data.Time.Clock.POSIX
+import Chopaan.Comm.S3
+
+spec :: Spec
+spec = describe "S3 Hydration Checks" $ do
+  it "length corresponds to time units" $ do
+    sec' <- S.length xs
+    -- m' <- S.length ms
+    -- h' <- S.length hs
+    -- d' <- S.length ds
+    -- w' <- S.length ws
+    sec' `shouldBe` 8641
+    -- m' `shouldBe` 1440
+    -- h' `shouldBe` 24
+    -- d' `shouldBe` 1
+    -- w' `shouldBe` 1
+  it "prefix length is sane" $ do
+    sec' <- S.the $ fmap T.length xs
+    m' <- S.the $ fmap T.length ms
+    h' <- S.the $ fmap T.length hs
+    d' <- S.the $ fmap T.length ds
+    w' <- S.the $ fmap T.length ws
+    sec' `shouldBe` (Just 9)
+    m' `shouldBe` (Just 7)
+    h' `shouldBe` (Just 5) 
+    w' `shouldBe` (Just 3)
+
+
+unM (MilliSecond64 w') = w'
+w = unM worldStart
+s = posixSecondsToUTCTime . fromIntegral $ (div w  1000)
+e = posixSecondsToUTCTime . fromIntegral $ (div w  1000) + 86400
+xs = prefixRange Second s e
+ms = prefixRange Minute s e
+hs = prefixRange Hour s e
+ds = prefixRange Day s e
+ws = prefixRange Week s e
+
+--x' <- S.length xs
