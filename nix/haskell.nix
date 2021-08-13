6a7e6284190dbdda9a13d7ba9505db0e4baf3275
Faez Shakil <faez@ecoenergyfinance.org>
2021-08-13 20:57:17 +0500

    disable tests

--- nix/haskell.nix
+++ nix/haskell.nix
@@ -69,6 +69,7 @@ let
         packages.chopaan = {
           #components.tests.chopaan-tests.build-tools = [ ]; # jormungandr
           doCoverage = false;
+          doCheck = false;
           configureFlags = [
             "--extra-lib-dirs=${buildPackages.z3}/lib"
             "--extra-include-dirs=${buildPackages.z3}/include"
