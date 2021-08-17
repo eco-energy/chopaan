fba8066dc0d43b19b4ecefdd922f4e966c5f7393
Faez Shakil <faez@ecoenergyfinance.org>
2021-08-18 04:45:09 +0500

    get rid of this

--- src/Chopaan/Run.hs
+++ src/Chopaan/Run.hs
@@ -3,43 +3,43 @@
 {-# LANGUAGE RecordWildCards, NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
 {-# LANGUAGE MultiParamTypeClasses, GADTs, FlexibleInstances #-}
-module Chopaan.Run (run) where
+module Chopaan.Run where
 
-import GHC.Generics
-import Chopaan.Types
-import RIO hiding (view, async, withAsync, Async)
+-- import GHC.Generics
+-- import Chopaan.Types
+-- import RIO hiding (view, async, withAsync, Async)
 
-import Kbtz
-import Chopaan.Utils.Retry
-import Chopaan.Kibbutz
-import Chopaan.Kibbutz.KbtzId
-import Chopaan.Node.NodeId
-import Streamly
+-- import Kbtz
+-- import Chopaan.Utils.Retry
+-- import Chopaan.Kibbutz
+-- import Chopaan.Kibbutz.KbtzId
+-- import Chopaan.Node.NodeId
+-- import Streamly
 
 
-data ChopaanState = ChopaanState
-  { mqttClient :: Bool
-  , serverHandle :: Bool
-  } deriving (Eq, Ord, Show, Generic)
+-- data ChopaanState = ChopaanState
+--   { mqttClient :: Bool
+--   , serverHandle :: Bool
+--   } deriving (Eq, Ord, Show, Generic)
 
-data KbtzType = Monitor | Ui
+-- data KbtzType = Monitor | Ui
 
-run :: RIO App ()
-run = do
-  app <- ask
-  let
-    Options{..} = appOptions app
-    KibbutzOpts{..} = kibbutzOpts
-    kibbutzim = ["g"]
-  -- qs <-mkMqttQs mqttOpts{connId=n}
-  -- let  kbtzOpts =
-  --     fmap (\n -> mkKbtzConf (KbtzId n) [] (Left $ qs) janusHost janusPort) kibbutzim 
-  -- liftIO $ mapM_ runKibbutz kbtzOpts
-  --liftIO $ print ("Running Monitor...")
-  --liftIO $ mon id (defGrid nodes) sensors runtime
-  return ()
-  where
-    janusHost = "localhost"
-    janusPort = 8182
+-- run :: RIO App ()
+-- run = do
+--   app <- ask
+--   let
+--     Options{..} = appOptions app
+--     KibbutzOpts{..} = kibbutzOpts
+--     kibbutzim = ["g"]
+--   -- qs <-mkMqttQs mqttOpts{connId=n}
+--   -- let  kbtzOpts =
+--   --     fmap (\n -> mkKbtzConf (KbtzId n) [] (Left $ qs) janusHost janusPort) kibbutzim 
+--   -- liftIO $ mapM_ runKibbutz kbtzOpts
+--   --liftIO $ print ("Running Monitor...")
+--   --liftIO $ mon id (defGrid nodes) sensors runtime
+--   return ()
+--   where
+--     janusHost = "localhost"
+--     janusPort = 8182
 
-instance KbtzM (IO) NodeMAC
+-- instance KbtzM (IO) NodeMAC
