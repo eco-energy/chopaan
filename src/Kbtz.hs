7e9bfae418fbc8946203256550198e93fca8e7fa
Faez Shakil <faez@ecoenergyfinance.org>
2021-08-18 04:45:43 +0500

    get rid of this

--- src/Kbtz.hs
+++ src/Kbtz.hs
@@ -7,39 +7,39 @@
 
 module Kbtz where
 
-import Chopaan.Types
-import Chopaan.Comm.Comm (
-  Address(..)
-  , Dispatch
-  , MessageQs
-  , WriteChan
-  , PubQueue
-  , initMessageQs
-  , writeToPubQ)
-import Chopaan.Kibbutz.Kibbutz (sub, getNodes)
-import Chopaan.Node.NodeId
-import Chopaan.Kibbutz.KbtzId
+-- import Chopaan.Types
+-- import Chopaan.Comm.Comm (
+--   Address(..)
+--   , Dispatch
+--   , MessageQs
+--   , WriteChan
+--   , PubQueue
+--   , initMessageQs
+--   , writeToPubQ)
+-- import Chopaan.Kibbutz.Kibbutz (sub, getNodes)
+-- import Chopaan.Node.NodeId
+-- import Chopaan.Kibbutz.KbtzId
 
-import Streamly
-import qualified Streamly.Prelude as S
+-- import Streamly
+-- import qualified Streamly.Prelude as S
 
-import RIO
+-- import RIO
 
-class (MonadAsync m, Address n) => KbtzM m n | m -> n where
-  kbtzNodes :: KbtzName -> m [n]
-  kbtzQueues :: m (MessageQs n)
-  subscribe :: forall t a. (IsStream t, Dispatch a)
-    => WriteChan n a -> n -> m (t m a)
-  publish :: forall t a. (IsStream t, Dispatch a)
-    => PubQueue -> t m (n, a) -> m ()
-  save :: forall a. (Dispatch a) => (n, a) -> m ()
-  retrieve :: forall t a. (IsStream t, Dispatch a) => n -> t m a
+-- class (MonadAsync m, Address n) => KbtzM m n | m -> n where
+--   kbtzNodes :: KbtzName -> m [n]
+--   kbtzQueues :: m (MessageQs n)
+--   subscribe :: forall t a. (IsStream t, Dispatch a)
+--     => WriteChan n a -> n -> m (t m a)
+--   publish :: forall t a. (IsStream t, Dispatch a)
+--     => PubQueue -> t m (n, a) -> m ()
+--   save :: forall a. (Dispatch a) => (n, a) -> m ()
+--   retrieve :: forall t a. (IsStream t, Dispatch a) => n -> t m a
 
 
-instance KbtzM (ReaderT App IO) NodeMAC where
-  kbtzNodes = liftIO . getNodes
-  kbtzQueues = liftIO initMessageQs
-  subscribe = sub
-  publish p s = S.mapM_ (\(n, a) -> liftIO $ writeToPubQ p (controlTopic n) a) $ adapt s
-  save = undefined
-  retrieve = undefined
+-- instance KbtzM (ReaderT App IO) NodeMAC where
+--   kbtzNodes = liftIO . getNodes
+--   kbtzQueues = liftIO initMessageQs
+--   subscribe = sub
+--   publish p s = S.mapM_ (\(n, a) -> liftIO $ writeToPubQ p (controlTopic n) a) $ adapt s
+--   save = undefined
+--   retrieve = undefined
